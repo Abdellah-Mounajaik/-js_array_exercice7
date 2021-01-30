df3ca365b56cdd4de62e932758bfcf9f05b1e247
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 00:59:26 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+let classe = ["zak", "achraf", "ayhan", "abdel"];
+console.log(classe);
+console.log(prenom[3]);
+console.log(classe.length);
+
+let monPrenom = ["abdel"];
+console.log(monPrenom);
+monPrenom.push("prenom2");
+console.log(monPrenom);
+
+let coding16 = ["ayhan"];
+console.log(`${coding16} arrive en classe`);
+coding16.push("Bendedicte");
+console.log(`${coding16} rentre en classe`);
+coding16.push("louise", "jamila");
+coding16.push("sebastien", "adame");
+coding16.push("dawid", "malo");
+coding16.push("alex");
+console.log(coding16);
+coding16.splice(8, 1);
+console.log(coding16);
+coding16.push("maxence", "mohammed");
+coding16.push("saliou");
+coding16.push("zulma", "maximilien");
+coding16.push("zakaria", "ayoub");
+console.log(coding16);
+coding16.splice(13, 2);
+console.log(coding16);
+coding16.push("achraf");
+coding16.push("abdellah");
